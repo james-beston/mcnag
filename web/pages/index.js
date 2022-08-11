c514dd59959f7f0e3c4968085989f640ada09c88
James Beston <james@beston.me>
2022-08-11 17:49:22 +0100

    Update index.js

--- web/pages/index.js
+++ web/pages/index.js
@@ -2,14 +2,22 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full bg-gradient-to-b from-slate-600 to-slate-900">
+    <div className='h-screen'>
       <Head>
         <title>MCNAG - Marine Conservation for Norfolk Action Group</title>
         <meta name="description" content="Marine Conservation for Norfolk Action Group website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      
+      <div className="flex h-1/2 lg:h-full flex-col w-full bg-gradient-to-b from-mcnag-light-blue to-mcnag-dark-blue">
+        <div className="relative w-full h-full">
+          <div className="absolute bottom-0 left-0 w-full">
+            <img src="/wave.svg" alt="" className="min-w-full h-auto" />
+          </div>
+        </div>
+      </div>
+      <div className="flex">
+        More info here
+      </div>
     </div>
   )
 }
