f0410eeb44613eeaedd55ec0d4e9177e2dff0f46
James Beston <james@beston.me>
2022-08-11 17:25:00 +0100

    Begin layout of home page

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang='en'>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
           rel="stylesheet"
