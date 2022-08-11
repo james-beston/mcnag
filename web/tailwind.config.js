54b307f09958fde8e27781f6e836068b81a82874
James Beston <james@beston.me>
2022-08-11 17:15:32 +0100

    new _document and Inter fon

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -5,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
