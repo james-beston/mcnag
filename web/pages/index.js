f7b8c212e8fe3df906f83b924abfdab56613e897
James Beston <james@beston.me>
2022-08-22 16:10:10 +0100

    Create hero

--- web/pages/index.js
+++ web/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <meta name="description" content="Marine Conservation for Norfolk Action Group website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex h-2/3 lg:h-full flex-col w-full bg-gradient-to-b from-mcnag-light-blue to-mcnag-dark-blue">
+      <div className="flex h-3/4 md:h-2/3 lg:h-full flex-col w-full bg-gradient-to-b from-mcnag-light-blue to-mcnag-dark-blue">
         <div className="relative w-full h-full">
           <div className="absolute bottom-0 left-0 w-1/2">
             <img src="/wave-left.svg" alt="" className="h-full w-auto" />
@@ -16,8 +16,8 @@ export default function Home() {
           <div className="absolute bottom-0 right-0 w-1/2">
             <img src="/wave-right.svg" alt="" className="float-right h-full w-auto" />
           </div>
-          <div className="flex flex-col items-start w-full lg:w-1/3 mt-36 lg:mt-48 xl:mt-64 ml-8 lg:ml-12 xl:ml-32">
-          <img src="/mcnag-name.svg" alt="Marine Conservation Norfolk Action Group" className="h-48 lg:h-96 w-auto" />
+          <div className="flex flex-col items-start max-w-full mt-12 md:mt-36 xl:mt-48 xl:mt-64 ml-6 lg:ml-12 xl:ml-32">
+          <img src="/mcnag-name.svg" alt="Marine Conservation Norfolk Action Group" width={1024} height={548} className="h-auto w-2/3 xl:w-2/5" />
           </div>
         </div>
       </div>
