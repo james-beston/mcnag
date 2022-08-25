314281e478a926dca0179e15bdccb6f2a2b514b3
James Beston <james@beston.me>
2022-08-25 17:38:16 +0100

    Style up the nav

---
+++ web/components/dropdown.js
@@ -0,0 +1,62 @@
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Dropdown({ props }) {
+  console.log(props)
+  return (
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={classNames(
+              open ? 'text-blue-100' : 'text-white',
+              'group inline-flex items-center text-sm font-medium text-white hover:text-blue-100 focus:outline-none'
+            )}
+          >
+            <span>{props.name}</span>
+            <ChevronDownIcon
+              className={classNames(
+                open ? 'text-blue-100' : 'text-white',
+                'ml-2 h-5 w-5 group-hover:text-blue-100'
+              )}
+              aria-hidden="true"
+            />
+          </Popover.Button>
+
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-150"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
+              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+                <div className="relative grid gap-6 bg-blue-50 px-5 py-6 sm:gap-8 sm:p-8">
+                  {props.list.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-m-3 p-3 flex items-start rounded-lg hover:bg-blue-100"
+                    >
+                      <div className="ml-0">
+                        <p className="text-sm font-medium text-mcnag-dark-blue">{item.name}</p>
+                      </div>
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
+  )
+}
--- web/components/navigation.js
+++ web/components/navigation.js
@@ -1,15 +1,25 @@
-import { Fragment } from 'react';
-import { Popover, Transition } from '@headlessui/react';
-import { MenuIcon, XIcon } from '@heroicons/react/outline';
+import { Fragment } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Popover, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import Dropdown from './dropdown'
 
 const navigation = [
-  { name: 'Contact', href: '#' },
-  { name: 'Local Marine Information', href: '#' },
-  { name: 'The Chalk Reef MCZ', href: '#' },
-  { name: 'How To Help', href: '#' },
-  { name: 'Norfolk Beach Cleaners', href: '#' },
-  { name: 'Conference', href: '#' },
-  { name: 'Reports', href: '#' },
+  { name: 'Contact', type: 'single', href: '#' },
+  { name: 'Marine Information',
+    type: 'list',
+    list: [
+      { 'name': 'Local Marine Biology', href: '#' },
+      { 'name': 'Local Marine Geography', href: '#' },
+      { 'name': 'The Chalk Reef Marine Conservation Zone', href: '#' },
+      { 'name': 'Local Concerns', href: '#' },
+      { 'name': 'Norfolk Beach Cleaners Collective', href: '#' }
+    ]
+  },
+  { name: 'How To Help', type: 'single', href: '#' },
+  { name: 'Conference', type: 'single', href: '#' },
+  { name: 'Reports', type: 'single', href: '#' },
 ]
 
 export default function Navigation() {
@@ -23,14 +33,16 @@ export default function Navigation() {
           >
             <div className="flex items-center flex-1">
               <div className="flex items-center justify-between w-full md:w-auto">
-                <a href="#">
-                  <span className="sr-only">Workflow</span>
-                  <img
-                    className="h-8 w-auto sm:h-10"
-                    src="/android-chrome-192x192.png"
-                    alt=""
-                  />
-                </a>
+                <Link href="/">
+                  <a className="h-8 sm:h-10 w-8 sm:w-10">
+                    <Image
+                      src="/android-chrome-192x192.png"
+                      alt="Marine Conservation Norfolk Action Group"
+                      width={192}
+                      height={192}
+                    />
+                  </a>
+                </Link>              
                 <div className="-mr-2 flex-0 items-center md:hidden">
                   <Popover.Button className="bg-black/30 rounded-md p-2 inline-flex items-center justify-center text-white hover:bg-black/50 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
@@ -38,11 +50,16 @@ export default function Navigation() {
                   </Popover.Button>
                 </div>
               </div>
-              <div className="hidden space-x-8 md:flex md:ml-10">
+              <div className="hidden space-x-8 md:flex md:ml-10 items-center">
                 {navigation.map((item) => (
-                  <a key={item.name} href={item.href} className="text-sm font-medium text-white hover:text-blue-200">
-                    {item.name}
-                  </a>
+                  item.type === 'single'
+                  ? (
+                    <Link key={item.name} href={item.href}>
+                      <a className="text-sm font-medium text-white hover:text-blue-100">
+                        {item.name}
+                      </a>
+                    </Link>
+                  ) : <Dropdown props={item} />
                 ))}
               </div>
             </div>
@@ -61,13 +78,16 @@ export default function Navigation() {
           <Popover.Panel focus className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
             <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div className="px-5 pt-4 flex items-center justify-between">
-                <div>
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600"
-                    alt=""
-                  />
-                </div>
+                <Link href="/">
+                  <a className="h-8 sm:h-10 w-8 sm:w-10">
+                    <Image
+                      src="/android-chrome-192x192.png"
+                      alt="Marine Conservation Norfolk Action Group"
+                      width={192}
+                      height={192}
+                    />
+                  </a>
+                </Link>    
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
                     <span className="sr-only">Close menu</span>
@@ -78,30 +98,30 @@ export default function Navigation() {
               <div className="pt-5 pb-6">
                 <div className="px-2 space-y-1">
                   {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
+                    item.type === 'single'
+                  ? (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-blue-50"
+                      >
+                        {item.name}
+                      </a>
+                  ) : (
+                    <>
+                    <p className="block px-3 py-2 rounded-md text-base font-medium text-gray-500">{item.name}</p>
+                    {item.list.map((nav) => (
+                      <a
+                        key={nav.name}
+                        href={nav.href}
+                        className="block mx-2 px-3 py-1.5 rounded-md text-sm text-mcnag-dark-blue hover:bg-blue-50"
+                      >
+                        {nav.name}
+                      </a>
+                    ))}
+                    </>
+                  )
                   ))}
-                </div>
-                <div className="mt-6 px-5">
-                  <a
-                    href="#"
-                    className="block text-center w-full py-3 px-4 rounded-md shadow bg-indigo-600 text-white font-medium hover:bg-indigo-700"
-                  >
-                    Start free trial
-                  </a>
-                </div>
-                <div className="mt-6 px-5">
-                  <p className="text-center text-base font-medium text-gray-500">
-                    Existing customer?{' '}
-                    <a href="#" className="text-gray-900 hover:underline">
-                      Login
-                    </a>
-                  </p>
                 </div>
               </div>
             </div>
