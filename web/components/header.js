931146488c0546f91c6d3056e23237ee617164f9
James Beston <james@beston.me>
2022-08-25 16:21:47 +0100

    CReate HEader component
    And fix issues with screen resolutions + use Net Image

---
+++ web/components/header.js
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+
+export default function Header() {
+  return (
+    <div className="flex h-full flex-col w-full bg-gradient-to-b from-mcnag-light-blue to-mcnag-dark-blue">
+      <div className="flex mt-56 lg:mt-0 lg:items-center w-full h-full">
+        <div className="absolute bottom-0 left-0 w-1/2 lg:w-1/3" aria-hidden="true">
+          <Image
+            src="/wave-left.svg"
+            alt=""
+            width={685}
+            height={419}
+            layout="responsive"
+          />
+        </div>
+        <div className="absolute bottom-0 right-0 w-1/2 lg:w-1/3" aria-hidden="true">
+        <Image
+            src="/wave-right.svg"
+            alt=""
+            width={755}
+            height={870}
+            layout="responsive"
+          />
+        </div>
+        <div className="w-full md:w-1/2 mx-6 lg:ml-24 xl:ml-54 2xl:ml-64">
+          <Image
+            src="/mcnag-name.svg"
+            alt="Marine Conservation Norfolk Action Group"
+            width={640}
+            height={360}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
--- web/pages/index.js
+++ web/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import Navigation from '../components/navigation';
+import Navigation from '@components/navigation';
+import Header from '@components/header';
 
 export default function Home() {
   return (
@@ -12,19 +13,7 @@ export default function Home() {
       <div className="sticky top-0 z-10">
         <Navigation />
       </div>
-      <div className="flex h-3/4 md:h-2/3 lg:h-full flex-col w-full bg-gradient-to-b from-mcnag-light-blue to-mcnag-dark-blue">
-        <div className="relative w-full h-full">
-          <div className="absolute bottom-0 left-0 w-1/2">
-            <img src="/wave-left.svg" alt="" className="h-full w-auto" />
-          </div>
-          <div className="absolute bottom-0 right-0 w-1/2">
-            <img src="/wave-right.svg" alt="" className="float-right h-full w-auto" />
-          </div>
-          <div className="flex flex-col items-start max-w-full mt-12 md:mt-36 xl:mt-48 xl:mt-64 ml-6 lg:ml-12 xl:ml-32">
-          <img src="/mcnag-name.svg" alt="Marine Conservation Norfolk Action Group" width={1024} height={548} className="h-auto w-2/3 xl:w-2/5" />
-          </div>
-        </div>
-      </div>
+      <Header />
       <div className="flex">
         More info here
       </div>
