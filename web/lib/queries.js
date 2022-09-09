09b6b1eed885bda1539511767d2b879e9f4f2ad5
James Beston <james@beston.me>
2022-09-09 17:54:16 +0100

    Add About page query

--- web/lib/queries.js
+++ web/lib/queries.js
@@ -1,11 +1,20 @@
 import { groq } from "next-sanity"
 
 export const homeQuery = groq`
-*[_type == 'home'] {
-  _id,
-  title,
-  mainImage,
-  mainImageCredit,
-  content
-}
+  *[_type == 'home'] {
+    _id,
+    title,
+    mainImage,
+    mainImageCredit,
+    content
+  }
+`
+
+export const aboutQuery = groq`
+  *[_type == 'about']{
+    title,
+    "slug": slug.current,
+    introduction,
+    members
+  }
 `
